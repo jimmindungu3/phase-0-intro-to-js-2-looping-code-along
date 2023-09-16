b66d526d72d2e53361b04608cad0b6a42022e378
jamesndungu <jimmindungu3@gmail.com>
2023-09-16 20:45:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const messages = []
+
+function writeCards(names, event) {
+    for (let i = o; names.lenght > i; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown() {
+    let count = 0;
+    while (count < 11) {
+        console.log(count++);
+    }
+}
